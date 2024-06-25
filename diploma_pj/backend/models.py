9c19bea3c39cae07600ddc0c2a65adfbcc935685
ivan <timofeevis89@gmail.com>
2024-06-25 14:42:43 +0300

    add Black hook

--- diploma_pj/backend/models.py
+++ diploma_pj/backend/models.py
@@ -2,109 +2,149 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS_CHOICES = (
-    ('new', 'New'),
-    ('confirmed', 'Confirmed'),
-    ('assembled', 'Assembled'),
-    ('sent', 'Sent'),
-    ('delivered', 'Delivered'),
-    ('canceled', 'Canceled'),
-    ('returned', 'Returned'),
+    ("new", "New"),
+    ("confirmed", "Confirmed"),
+    ("assembled", "Assembled"),
+    ("sent", "Sent"),
+    ("delivered", "Delivered"),
+    ("canceled", "Canceled"),
+    ("returned", "Returned"),
 )
 
 CONTACT_TYPES = (
-    ('shop', 'Shop'),
-    ('buyer', 'Buyer'),
+    ("shop", "Shop"),
+    ("buyer", "Buyer"),
 )
 
+
 class Shop(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Shop name')
-    url = models.URLField(null=True, blank=True, verbose_name='Shop URL')
+    name = models.CharField(max_length=100, verbose_name="Shop name")
+    url = models.URLField(null=True, blank=True, verbose_name="Shop URL")
 
     class Meta:
-        verbose_name = 'Shop'
+        verbose_name = "Shop"
         verbose_name_plural = "List of shops"
-        ordering = ('-name',)
+        ordering = ("-name",)
 
 
 class Category(models.Model):
-    shops = models.ManyToManyField(Shop, verbose_name='Shops', related_name='categories')
-    name = models.CharField(max_length=100, verbose_name='Category name')
+    shops = models.ManyToManyField(
+        Shop, verbose_name="Shops", related_name="categories"
+    )
+    name = models.CharField(max_length=100, verbose_name="Category name")
 
     class Meta:
-        verbose_name = 'Category'
+        verbose_name = "Category"
         verbose_name_plural = "List of categories"
-        ordering = ('-name',)
+        ordering = ("-name",)
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    name = models.CharField(max_length=100, verbose_name='Product name')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    name = models.CharField(max_length=100, verbose_name="Product name")
 
     class Meta:
-        verbose_name = 'Product'
+        verbose_name = "Product"
         verbose_name_plural = "List of products"
-        ordering = ('-name',)
+        ordering = ("-name",)
+
 
 class ProductInfo(models.Model):
-     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Product')
-     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, verbose_name='Shop')
-     name = models.CharField(max_length=100, verbose_name='Product name')
-     quantity = models.PositiveIntegerField(verbose_name='Quantity')
-     price = models.PositiveIntegerField(verbose_name='Price')
-     price_rrc = models.PositiveIntegerField(null=True, blank=True, verbose_name='Recommended retail price')
-     class Meta:
-        verbose_name = 'Product information'
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, verbose_name="Product"
+    )
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, verbose_name="Shop")
+    name = models.CharField(max_length=100, verbose_name="Product name")
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
+    price = models.PositiveIntegerField(verbose_name="Price")
+    price_rrc = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name="Recommended retail price"
+    )
+
+    class Meta:
+        verbose_name = "Product information"
         verbose_name_plural = "List of products information"
-        ordering = ('-name',)
+        ordering = ("-name",)
+
 
 class Parameter(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Parameter name')
+    name = models.CharField(max_length=100, verbose_name="Parameter name")
 
     class Meta:
-        verbose_name = 'Parameter'
+        verbose_name = "Parameter"
         verbose_name_plural = "List of parameters"
-        ordering = ('-name',)
+        ordering = ("-name",)
+
 
 class ProductParameter(models.Model):
-    product_info = models.ForeignKey(ProductInfo, on_delete=models.CASCADE, verbose_name='Product information', related_name='parameters')
-    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE, verbose_name='Parameter', related_name='product_parameters')
-    value = models.CharField(max_length=100, verbose_name='Parameter value')
+    product_info = models.ForeignKey(
+        ProductInfo,
+        on_delete=models.CASCADE,
+        verbose_name="Product information",
+        related_name="parameters",
+    )
+    parameter = models.ForeignKey(
+        Parameter,
+        on_delete=models.CASCADE,
+        verbose_name="Parameter",
+        related_name="product_parameters",
+    )
+    value = models.CharField(max_length=100, verbose_name="Parameter value")
 
     class Meta:
-        verbose_name = 'Product parameter'
+        verbose_name = "Product parameter"
         verbose_name_plural = "List of product parameters"
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User', related_name='orders')
-    date = models.DateField(verbose_name='Date', auto_now_add=True)
-    status = models.CharField(choises=STATUS_CHOICES, default='new', verbose_name='Status', max_length=20)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="User", related_name="orders"
+    )
+    date = models.DateField(verbose_name="Date", auto_now_add=True)
+    status = models.CharField(
+        choises=STATUS_CHOICES, default="new", verbose_name="Status", max_length=20
+    )
 
     class Meta:
-        verbose_name = 'Order'
+        verbose_name = "Order"
         verbose_name_plural = "List of orders"
-        ordering = ('-date',)
+        ordering = ("-date",)
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Order', related_name='order_items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Product', related_name='order_items')
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, verbose_name='Shop', related_name='shop_items')
-    quantity = models.PositiveIntegerField(verbose_name='Quantity')
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        verbose_name="Order",
+        related_name="order_items",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name="Product",
+        related_name="order_items",
+    )
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, verbose_name="Shop", related_name="shop_items"
+    )
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
 
     class Meta:
-        verbose_name = 'Ordered item'
+        verbose_name = "Ordered item"
         verbose_name_plural = "List of ordered items"
 
 
 class Contact(models.Model):
-    type = models.CharField(choices=CONTACT_TYPES, verbose_name='Type', max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User', related_name='contacts')
-    name = models.CharField(max_length=100, verbose_name='Name')
-    phone = models.CharField(max_length=20, verbose_name='Phone')
-
+    type = models.CharField(choices=CONTACT_TYPES, verbose_name="Type", max_length=20)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="User", related_name="contacts"
+    )
+    name = models.CharField(max_length=100, verbose_name="Name")
+    phone = models.CharField(max_length=20, verbose_name="Phone")
 
     class Meta:
-        verbose_name = 'Contact'
+        verbose_name = "Contact"
         verbose_name_plural = "List of contacts"
-        ordering = ('-name',)
+        ordering = ("-name",)
--- diploma_pj/diploma_pj/settings.py
+++ diploma_pj/diploma_pj/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ubm_674#sqzbyyq+41r36@pguswe@6x-c_0(vj^6032500u11i'
+SECRET_KEY = "django-insecure-ubm_674#sqzbyyq+41r36@pguswe@6x-c_0(vj^6032500u11i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv("DEBUG", "False")
 
 ALLOWED_HOSTS = []
 
@@ -35,60 +35,59 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-
-    'backend',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "backend",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'diploma_pj.urls'
+ROOT_URLCONF = "diploma_pj.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'diploma_pj.wsgi.application'
+WSGI_APPLICATION = "diploma_pj.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('POSTGRES_DB', 'netology'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'USER': os.getenv('POSTGRES_USER', 'netology'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'secret'),
+    "default": {
+        "ENGINE": os.getenv("ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("POSTGRES_DB", "netology"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "USER": os.getenv("POSTGRES_USER", "netology"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "secret"),
     }
 }
 
@@ -98,16 +97,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -115,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -127,18 +126,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
