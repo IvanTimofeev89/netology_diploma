a3342675dcb8653e483704495f722b14ac9f035a
ivan <timofeevis89@gmail.com>
2024-07-18 17:35:21 +0300

    docker files changed

--- diploma_pj/run.sh
+++ diploma_pj/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 wait-for-it postgres_db:5432 -t 30
-python manage.py collectstatic --noinput
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py create_superuser
